833a88ea988d8a53d7a3454e23c5a722dd886c05
younesbl67 <63188432+younesbl67@users.noreply.github.com>
2020-04-05 12:45:11 +0200

    Mise en place de la structure générale
    Mise en place de la page d'accueil (html/css)
    Mise en place des images

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBAF - Accueil</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="topbar">
+        <a href="index.php" class="logo-GBAF"><img src="img/GBAF.png" alt="Logo du GBAF"></a>
+        <a href="#" class="user">Exemple</a>
+    </header>
+    <section class="main">
+        <header class="presentation">
+            <h1>Le Groupement Banque-Assurance Français</h1>
+            <p>Fondé en 2010 par Emmanuel Conrad, le Groupement Banque-Assurance Français (GBAF) est une fédération représentant 6 grands groupes français du monde bancaire. Sa mission ? Promouvoir l'activité bancaire à l’échelle nationale.</p>
+            <img src="img/banniere.jpg" alt="Une banque d'affaires">
+        </header>
+        <section class="partners">
+            <header>
+                <h2>Nos partenaires et acteurs</h2>
+                <p>Le GBAF vous propose un point d’entrée unique, répertoriant un grand nombre d’informations sur les partenaires et acteurs du groupe ainsi que sur les produits et services bancaires et financiers. Retrouvez ci-dessous la liste de nos partenaires et acteurs.</p>
+            </header>
+            <div class="container">
+            
+                    <article class="partner partner-padding">
+                        <figure>
+                            <img src="img/CDE.png" class="partner-image" alt="cde">
+                            <figcaption class="partner-infos">
+                                <h3 class="partner-title">Nom du partner</h3>
+                                <p class="partner-description">Description</p>
+                            </figcaption>
+                        </figure>
+                        <a href="partner.php" class="partner-button">Lire la suite</a>
+                    </article>
+                
+            </div>
+        </section>  
+        <footer class="footer">
+            <a href="#" class="mentions">Mentions légales</a>
+            <a href="#" class="contact">Contact</a>
+        </footer>
+    </section>
+</body>
+</html>
